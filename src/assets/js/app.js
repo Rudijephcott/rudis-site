a9b241bc09ad14646c1589d56007979b8bac6db5
Rudi Jephcott <rudijephcott@gmail.com>
2022-08-19 17:07:14 +0200

    final shmexy

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,3 +3,16 @@ document.addEventListener("keyup", function (e) {
     document.querySelector(".grid-overlay").classList.toggle("show");
   }
 });
+
+$(document).ready(function() {
+  $('.nav-bar a').click(function(event) {
+    let targetElement = $(this.hash);
+    if (targetElement.length) {
+    event.preventDefault();
+    let targetCalc = targetElement.offset().top - 50;
+    $('html, body').animate({
+      scrollTop: targetCalc
+    }, 1000);
+  }});
+
+});
